bbb7f370efe37ea6ecdd32ab591e4a1e1bee0e66
Jordi C. <jordicunillera1@gmail.com>
2025-07-23 18:27:07 +0200

    application.yml:  - Swagger, Mongo and Kafa config (no Imp)
    docker-compose.yml:
     - Launching kafka, mongo and zookeeper
    
    HotelAvailabilityApplication.java:
     - Adding @SpringBootApplication

--- src/main/java/com/avoristech/hotelavailability/HotelAvailabilityApplication.java
+++ src/main/java/com/avoristech/hotelavailability/HotelAvailabilityApplication.java
@@ -3,7 +3,7 @@ package com.avoristech.hotelavailability;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class HotelAvailabilityApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelAvailabilityApplication.class, args);

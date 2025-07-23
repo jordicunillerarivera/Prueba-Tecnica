88f6d4ab5205a55e3a767d24ed8e035041cfa1bf
Jordi C. <jordicunillera1@gmail.com>
2025-07-23 17:26:07 +0200

    Initial config: - Maven Config - SpringBoot - Dependencies - Plugins

---
+++ src/main/java/com/avoristech/hotelavailability/HotelAvailabilityApplication.java
@@ -0,0 +1,11 @@
+package com.avoristech.hotelavailability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class HotelAvailabilityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelAvailabilityApplication.class, args);
+    }
+}

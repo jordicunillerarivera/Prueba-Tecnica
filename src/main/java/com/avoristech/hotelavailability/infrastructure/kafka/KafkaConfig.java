31916c4898af8d02d208af7404846c829c1d44cf
Jordi C. <jordicunillera1@gmail.com>
2025-07-25 01:26:35 +0200

    fix bootstrap-server on KafkaConfig

--- src/main/java/com/avoristech/hotelavailability/infrastructure/kafka/KafkaConfig.java
+++ src/main/java/com/avoristech/hotelavailability/infrastructure/kafka/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.avoristech.hotelavailability.infrastructure.kafka;
 import com.avoristech.hotelavailability.domain.model.Search;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,11 +16,14 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, Search> producerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "${spring.kafka.bootstrap-servers}");
+                bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
